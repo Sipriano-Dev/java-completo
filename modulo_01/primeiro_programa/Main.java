bde5f802fde61025dfbb9fe900ff0b1a8b50d239
Anderson Sipriano <Andsipriano@gmail.com>
2025-04-08 10:51:55 -0300

    First Program

--- modulo_01/primeiro_programa/Main.java
+++ modulo_01/primeiro_programa/Main.java
@@ -1,6 +1,12 @@
+import javax.swing.JFrame;
 	
-	public class Main {
-	
-		
-	
-	}
+public class Main {
+
+		public static void main(String[] args) {
+				
+				JFrame janela = new JFrame();
+				janela.setTitle("Meu Primeiro Programa");
+				janela.setSize(600, 400);
+				janela.setVisible(true);
+		}
+}
