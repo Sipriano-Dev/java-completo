4fbdc126f94551a71f524822cf737fa11f95e9fa
Anderson Sipriano <Andsipriano@gmail.com>
2025-04-09 09:07:47 -0300

    Repetition loops with WHILE

---
+++ modulo_03/primeiro_programa/src/LoopWhile.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class LoopWhile {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int contador = scan.nextInt();
+        int valor = 0;
+        while (valor <= contador) {
+            System.out.println("Número " + valor);
+            valor = valor + 1;
+        }
+    }
+}
