c7b940099a97c7ea6e4c68f9000d081cd605e36e
Anderson Sipriano <Andsipriano@gmail.com>
2025-04-09 08:25:37 -0300

    Other forms of IF ELSE and exercises

--- modulo_02/primeiro_programa/src/CalculadoraIdade.java
+++ modulo_02/primeiro_programa/src/CalculadoraIdade.java
@@ -18,8 +18,19 @@ public class CalculadoraIdade {
         int anoNascimento = teclado.nextInt();
 
         int resultado = anoAtual - anoNascimento;
-
         System.out.println("A idade é " + resultado);
+
+        if (resultado < 18) {
+            System.out.println("Você é menor de idade");
+        } else if (resultado > 60) {
+            System.out.println("Você é Idoso");
+        } else {
+            System.out.println("Você é adulto");
+        }
+
+        if (resultado < 12) {
+            System.out.println("Você é Criança");
+        }
 
         teclado.close();
     }
