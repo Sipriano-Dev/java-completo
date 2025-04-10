f6da7c22d1cd974e83ae8935d8428697b75bd298
Anderson Sipriano <Andsipriano@gmail.com>
2025-04-10 08:45:53 -0300

    adding behavior to objects through Methods

--- modulo_06/poo/src/Carro.java
+++ modulo_06/poo/src/Carro.java
@@ -4,5 +4,25 @@ public class Carro {
     String cor;
     String transmissao;
     String montadora;
+    int capacidadeTanque;
+    int combustivel;
+    int kmPorLitro;
+
+    void abastecer(int litros) {
+        int capacidadeAtual = capacidadeTanque - combustivel;
+        if (litros >= capacidadeAtual) {
+            System.out.println("Foi abastecido somente a capacidade do tanque : " + capacidadeTanque);
+            System.out.println("Abastecido: " + capacidadeAtual);
+            System.out.println("Sobrou: " + (litros - capacidadeAtual));
+            litros = capacidadeAtual;
+        }
+        combustivel += litros;
+
+    }
+
+    int autonomiaCombustivel() {
+        int autonomia = combustivel * kmPorLitro;
+        return autonomia;
+    }
 
 }
--- modulo_06/poo/src/MainCarro.java
+++ modulo_06/poo/src/MainCarro.java
@@ -6,14 +6,36 @@ public class MainCarro {
         punto.cor = "Preto";
         punto.montadora = "Fiat";
         punto.transmissao = "Automática";
+        punto.capacidadeTanque = 50;
+        punto.combustivel = 0;
+        punto.kmPorLitro = 10;
 
         Carro civic = new Carro();
         civic.cavalos = 150;
         civic.cor = "Prata";
         civic.montadora = "Honda";
         civic.transmissao = "Automática";
+        civic.capacidadeTanque = 48;
+        civic.combustivel = 10;
+        civic.kmPorLitro = 8;
 
-        System.out.println("Cavalos do punto: " + punto.cavalos);
-        System.out.println("Transmissão do civic: " + civic.transmissao);
+        System.out.println("Combustível do punto: " + punto.combustivel);
+        System.out.println("Combustível do civic: " + civic.combustivel);
+        System.out.println();
+
+        System.out.println("Após abastecimento: ");
+
+        punto.abastecer(70);
+        System.out.println("Combustível do punto: " + punto.combustivel);
+
+        civic.abastecer(30);
+        System.out.println("Combustível do civic: " + civic.combustivel);
+
+        System.out.println();
+
+        System.out.print("Punto autonomia: " + punto.autonomiaCombustivel() + "Km");
+        System.out.println();
+        System.out.print("Civic autonomia: " + civic.autonomiaCombustivel() + "Km");
+
     }
 }
