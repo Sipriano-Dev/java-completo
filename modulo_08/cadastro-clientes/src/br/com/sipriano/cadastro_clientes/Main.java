fbddabaef77143b13d50c2d0a7bbf8922576eb14
Anderson Sipriano <Andsipriano@gmail.com>
2025-04-12 14:08:37 -0300

    Adding fields to the screen

--- modulo_08/cadastro-clientes/src/br/com/sipriano/cadastro_clientes/Main.java
+++ modulo_08/cadastro-clientes/src/br/com/sipriano/cadastro_clientes/Main.java
@@ -1,7 +1,7 @@
 package br.com.sipriano.cadastro_clientes;
 
 import br.com.sipriano.cadastro_clientes.apresentacao.TelaCadastro;
-import br.com.sipriano.cadastro_clientes.dominio.Cliente;
+
 
 public class Main {
     public static void main(String[] args) {
--- modulo_08/cadastro-clientes/src/br/com/sipriano/cadastro_clientes/apresentacao/TelaCadastro.java
+++ modulo_08/cadastro-clientes/src/br/com/sipriano/cadastro_clientes/apresentacao/TelaCadastro.java
@@ -8,6 +8,10 @@ public class TelaCadastro extends JFrame {
     private JLabel labelCPF;
     private JLabel labelSexo;
 
+    private JTextField campoNome;
+    private JTextField campoCPF;
+    private JComboBox<String> campoSexo;
+
     public TelaCadastro() {
         construirTela();
     }
@@ -17,7 +21,47 @@ public class TelaCadastro extends JFrame {
         setTitle("Cadastro de cliente");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
+        this.adicionarCampos();
+        this.adicionarBotoes();
+        this.adicionarComponentesFoto();
     }
 
+    private void adicionarCampos() {
+        //Nome
+        labelNome = new JLabel("Nome: ");
+        labelNome.setBounds(20, 20, 200, 20);
+        getContentPane().add(labelNome);
+
+        campoNome = new JTextField();
+        campoNome.setBounds(20, 40, 200, 20);
+        getContentPane().add(campoNome);
+
+        //CPF
+        labelCPF = new JLabel("CPF: ");
+        labelCPF.setBounds(20, 60, 200, 20);
+        getContentPane().add(labelCPF);
+
+        campoCPF = new JTextField();
+        campoCPF.setBounds(20, 80, 200, 20);
+        getContentPane().add(campoCPF);
+
+        //Sexo
+        labelSexo = new JLabel("Sexo: ");
+        labelSexo.setBounds(20, 100, 200, 20);
+        getContentPane().add(labelSexo);
+
+        String[] tiposSexo = {"Masculino", "Feminino", "Outros"};
+        campoSexo = new JComboBox<>(tiposSexo);
+        campoSexo.setBounds(20, 120, 200, 20);
+        getContentPane().add(campoSexo);
+    }
+
+    private void adicionarBotoes() {
+
+    }
+
+    private void adicionarComponentesFoto() {
+
+    }
 
 }
