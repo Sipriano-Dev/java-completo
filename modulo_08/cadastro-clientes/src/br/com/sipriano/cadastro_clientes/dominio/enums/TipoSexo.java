80b21ca47134356bd88c5cce4cdc3589921b2b3a
Anderson Sipriano <Andsipriano@gmail.com>
2025-04-12 15:51:40 -0300

    Learning about Enums

---
+++ modulo_08/cadastro-clientes/src/br/com/sipriano/cadastro_clientes/dominio/enums/TipoSexo.java
@@ -0,0 +1,18 @@
+package br.com.sipriano.cadastro_clientes.dominio.enums;
+
+public enum TipoSexo {
+    M("Masculino"),
+    F("Feminino"),
+    O("Outros");
+
+    private String descricao;
+
+    TipoSexo(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    
+}
