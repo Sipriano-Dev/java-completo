0c00ed2d6ce92c06b65525506ed5b1d4519371de
Anderson Sipriano <Andsipriano@gmail.com>
2025-04-22 18:27:02 -0300

    Ler e deletar arquivos, criar pastas e outras operações

--- modulo_10/cadastro-clientes/src/br/com/sipriano/estudo/files/CriadorArquivo.java
+++ modulo_10/cadastro-clientes/src/br/com/sipriano/estudo/files/CriadorArquivo.java
@@ -1,22 +1,68 @@
 package br.com.sipriano.estudo.files;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class CriadorArquivo {
+
     public static void main(String[] args) {
+        CriadorArquivo criadorArquivo = new CriadorArquivo();
+        criadorArquivo.lerArquivoTexto();
+    }
+
+    private void lerArquivo() {
+        File arquivo = new File("D:\\extras\\arquivo.txt");
+        System.out.println("Caminho: " + arquivo.getAbsolutePath());
+        System.out.println("Nome: " + arquivo.getName());
+        System.out.println("Tamanho: " + arquivo.length());
+        System.out.println("É arquivo?: " + arquivo.isFile());
+    }
+
+    public void lerArquivoTexto(){
         try {
             File arquivo = new File("D:\\extras\\arquivo.txt");
+
+            FileReader fileReader = new FileReader(arquivo);
+            BufferedReader reader = new BufferedReader(fileReader);
+
+            String linha;
+
+            while ( (linha = reader.readLine()) != null ){
+                System.out.println(linha);
+            }
+
+            reader.close();
+
+        } catch (IOException e){
+
+        }
+
+    }
+
+    public void criarPasta() {
+        File file = new File("D:\\extras\\pasta");
+        boolean criou = file.mkdir();
+        System.out.println("Pasta criada? " + criou);
+    }
+
+    private void criarArquivo() {
+        try {
+            File arquivo = new File("D:\\extras\\arquivo2.txt");
             FileWriter fileWriter = new FileWriter(arquivo);
             BufferedWriter writer = new BufferedWriter(fileWriter);
 
-            writer.write("Escrevendo no novo arquivo");
+            writer.write("Escrevendo no novo arquivo a partir da ide");
             writer.flush();
             writer.close();
         } catch (IOException e) {
             System.out.println("Ocorreu um erro " + e.getMessage());
         }
     }
+
+    private void deletarArquivo() {
+        File arquivo = new File("D:\\extras\\arquivo2.txt");
+        boolean deletou = arquivo.delete();
+        System.out.println("Deletou? " + deletou);
+    }
+
+
 }
