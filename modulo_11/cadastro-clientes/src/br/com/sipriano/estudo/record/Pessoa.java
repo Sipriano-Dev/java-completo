a8c76f28d12c5856e67324e220675eb807067a63
Anderson Sipriano <Andsipriano@gmail.com>
2025-05-05 10:30:32 -0300

    Classes record ou data classes

---
+++ modulo_11/cadastro-clientes/src/br/com/sipriano/estudo/record/Pessoa.java
@@ -0,0 +1,21 @@
+package br.com.sipriano.estudo.record;
+
+public class Pessoa {
+
+    private final String nome;
+    private final String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+}
